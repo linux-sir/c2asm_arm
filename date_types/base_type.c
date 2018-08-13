30668b0f45f36f91d9d335c730bbe1c677a79c8b
Ma Guicai <guicai_ma@163.com>
2018-08-13 23:00:14 +0800

    Ｃ语言基本数据类型的机器级表示

---
+++ date_types/base_type.c
@@ -0,0 +1,33 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  base_type.c
+ *
+ *    Description:  C语言基本数据类型的机器级表示
+ *
+ *        Version:  1.0
+ *        Created:  2018年08月13日 22时22分53秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+int main(void)
+{
+    char s;
+    s = 12;
+    int a;
+    a = 2;
+    unsigned int ua;
+    ua = 23;
+
+    long la;
+    la = 243434324;
+
+    int *pa = &a;
+    return 0;
+}
+
