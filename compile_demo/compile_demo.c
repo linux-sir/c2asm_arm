b7cba9eb24f630251e4ba4e82490e41e05ec7fde
Ma Guicai <guicai_ma@163.com>
2018-08-13 22:57:46 +0800

    Ｃ代码的编译过程

---
+++ compile_demo/compile_demo.c
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  compile_demo.c
+ *
+ *    Description:  演示从C到机器码的演变过程
+ *
+ *        Version:  1.0
+ *        Created:  2018年08月13日 22时37分43秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(void)
+{
+    printf("compile demo !!!\n");
+    return 0;
+}
